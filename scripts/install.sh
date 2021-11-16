4ef2d5587199812fc5606239585bb4f5d37bb5b4
Steven McKeown <steven@pragmaflow.com>
2021-11-15 23:32:29 -0500

    Starting the injection of prefetch into ObjectRenderer

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,27 +1,3 @@
 
-# import the unsafeGetToken and AppProvider
-cat <<EOF > /tmp/adalo-app-sed
-/proton-runner/a\\
-import { AppProvider } from 'pf-adalo-data-preloader';\
-import { unsafeGetToken } from '@protonapp/proton-runner/lib/ducks/auth'\\
-EOF
-
-sed -i.bak "$(cat /tmp/adalo-app-sed)" App.js
-
-# wrap the ProtonRunner with the AppProvider
-cat <<EOF > /tmp/adalo-app-sed
-/return (/a\\
-		<AppProvider app={app} unsafeGetToken={unsafeGetToken}>\\
-EOF
-
-sed -i.bak "$(cat /tmp/adalo-app-sed)" App.js
-
-# close the AppProvider
-cat <<EOF > /tmp/adalo-app-sed
-/\/>/a\\
-		</AppProvider>\\
-EOF
-
-sed -i.bak "$(cat /tmp/adalo-app-sed)" App.js
-
-# now we need to export the tokens from proton-runner to access them
+cp ./node_modules/adalo-data-preloader/ObjectRenderer.js ./node_modules/@protonapp/proton-runner/lib/components/
+cp ./node_modules/adalo-data-preloader/ObjectRenderer.js.map ./node_modules/@protonapp/proton-runner/lib/components/
--- src/components/AdaloPreloader/index.js
+++ src/components/AdaloPreloader/index.js
@@ -6,7 +6,7 @@ import AdaloStoreComponent from './adaloStoreComponent'
 const AdaloPreloader = (props) => {
 	const { editor } = props
 	return(
-		<View>
+		<View style={{ height: editor ? props._height : 0, width: editor ? props._width : 0,  }}>
 			{
 				editor ?
 				<Text>Pre-loader installed</Text>
